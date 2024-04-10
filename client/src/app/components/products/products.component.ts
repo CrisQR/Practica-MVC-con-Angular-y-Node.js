3269f3d44b9ddc4349c20cac9d39f616900787cf
CrisQR <cdqr27@gmail.com>
2024-04-10 17:49:03 -0500

    Repairing client

--- client/src/app/components/products/products.component.ts
+++ client/src/app/components/products/products.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products.service'; 
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
 
+export class ProductsComponent implements OnInit {
+
+  listProducts: any = [];
+
+  constructor(private productsService: ProductsService) {
+
+  }
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productsService.listProducts().subscribe(
+      res => {
+        this.listProducts = res;
+      },
+      err => console.log(err)
+    )
+  }
 }
